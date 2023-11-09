67712c30c10ca2486842634f42205efa998fb9e7
JoseVides17 <jose.videsbaron@gmail.com>
2023-11-08 23:09:41 -0500

    mejorado

--- src/main/java/com/cootramixtol/sipd/services/asociado/AsociadoServiceImpl.java
+++ src/main/java/com/cootramixtol/sipd/services/asociado/AsociadoServiceImpl.java
@@ -1,10 +1,8 @@
 package com.cootramixtol.sipd.services.asociado;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import com.cootramixtol.sipd.entities.Asociado;
@@ -21,14 +19,18 @@ public class AsociadoServiceImpl implements AsociadoService{
 	@Override
 	public Asociado registrar(Asociado asociado) {
 
-		Asociado asociadoExiste = asociadoRepository.findByIdentificacion(asociado.getIdentificacion());
-		if(asociadoExiste.isActivo()){
-			throw new DuplicateKeyException("Este Asociado Existe...");
+		var existe = asociadoRepository.findByIdentificacion(asociado.getIdentificacion());
+		if (existe != null) {
+
+			return existe;
+			
 		}
 
-		Asociado asociadoExistePorCorreo = asociadoRepository.findByCorreo(asociado.getCorreo());
-		if (asociadoExistePorCorreo.isActivo()) {
-			throw new DuplicateKeyException("Este Asociado Existe...");
+		var existePorCorreo = asociadoRepository.findByCorreo(asociado.getCorreo());
+		if (existePorCorreo != null) {
+
+			return existePorCorreo;
+			
 		}
 
 		return asociadoRepository.save(asociado);
