305d47104a371fe7320ebfd0d9a2e3793426c16b
JoseVides17 <jose.videsbaron@gmail.com>
2023-11-13 12:14:37 -0500

    mapeo conductor y usuario

--- src/main/java/com/cootramixtol/sipd/mapper/Mapeador.java
+++ src/main/java/com/cootramixtol/sipd/mapper/Mapeador.java
@@ -3,8 +3,14 @@ package com.cootramixtol.sipd.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import com.cootramixtol.sipd.dtos.request.CrearConductorDtoReq;
+import com.cootramixtol.sipd.dtos.request.CrearUsuarioDtoReq;
 import com.cootramixtol.sipd.dtos.request.CrearVehiciculoDtoReq;
+import com.cootramixtol.sipd.dtos.response.CrearConductorDtoResp;
+import com.cootramixtol.sipd.dtos.response.CrearUsuarioDtoResp;
 import com.cootramixtol.sipd.dtos.response.CrearVehiculoDtoResp;
+import com.cootramixtol.sipd.entities.Conductor;
+import com.cootramixtol.sipd.entities.Usuario;
 import com.cootramixtol.sipd.entities.Vehiculo;
 
 @Mapper
@@ -12,7 +18,13 @@ public interface Mapeador {
     
       Mapeador INSTANCE = Mappers.getMapper(Mapeador.class);
 
-      CrearVehiculoDtoResp MapVehiculo(Vehiculo vehiculo);
-      Vehiculo MapVehiculo(CrearVehiciculoDtoReq crearVehiciculoDtoReq);
+      CrearVehiculoDtoResp mapVehiculo(Vehiculo vehiculo);
+      Vehiculo mapVehiculo(CrearVehiciculoDtoReq crearVehiciculoDtoReq);
+
+      CrearUsuarioDtoResp mapUsuario(Usuario usuarioGuardado);
+      Usuario mapUsuario(CrearUsuarioDtoReq crearUsuarioDtoReq);
+
+      CrearConductorDtoResp mapConductor(Conductor conductor);
+      Conductor mapConductor(CrearConductorDtoReq crearConductorDtoReq);
 
 }
--- src/main/java/com/cootramixtol/sipd/services/usuario/UsuarioServiceImpl.java
+++ src/main/java/com/cootramixtol/sipd/services/usuario/UsuarioServiceImpl.java
@@ -1,6 +1,5 @@
 package com.cootramixtol.sipd.services.usuario;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
@@ -12,6 +11,7 @@ import com.cootramixtol.sipd.dtos.request.CrearUsuarioDtoReq;
 import com.cootramixtol.sipd.dtos.response.CrearUsuarioDtoResp;
 import com.cootramixtol.sipd.entities.Rol;
 import com.cootramixtol.sipd.entities.Usuario;
+import com.cootramixtol.sipd.mapper.Mapeador;
 import com.cootramixtol.sipd.repositories.RolRepository;
 import com.cootramixtol.sipd.repositories.UsuarioRepository;
 
@@ -27,10 +27,10 @@ public class UsuarioServiceImpl implements UsuarioService{
 		
 
 	@Override
-	public CrearUsuarioDtoResp registrar(CrearUsuarioDtoReq crearUsuarioDto) {
+	public CrearUsuarioDtoResp registrar(CrearUsuarioDtoReq crearUsuarioDtoReq) {
 
-		var existe = usuarioRepository.findByIdentificacion(crearUsuarioDto.getIdentificacion());
-		var existePorCorreo = usuarioRepository.findByCorreo(crearUsuarioDto.getCorreo());
+		var existe = usuarioRepository.findByIdentificacion(crearUsuarioDtoReq.getIdentificacion());
+		var existePorCorreo = usuarioRepository.findByCorreo(crearUsuarioDtoReq.getCorreo());
 		
 		if (Objects.nonNull(existe) && Objects.nonNull(existePorCorreo)) {
 			
@@ -38,34 +38,15 @@ public class UsuarioServiceImpl implements UsuarioService{
 		}
 
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        crearUsuarioDto.setClave(bCryptPasswordEncoder.encode(crearUsuarioDto.getClave()));
+        crearUsuarioDtoReq.setClave(bCryptPasswordEncoder.encode(crearUsuarioDtoReq.getClave()));
 
-		Rol rol = rolRepository.findByDescripcion(crearUsuarioDto.getRol());
-		
+		Rol rol = rolRepository.findByDescripcion(crearUsuarioDtoReq.getRol());
 
-		Usuario usuario = new Usuario();
-
-		usuario.setIdentificacion(crearUsuarioDto.getIdentificacion());
-		usuario.setNombres(crearUsuarioDto.getNombres());
-		usuario.setApellidos(crearUsuarioDto.getApellidos());
+	    Usuario usuario = Mapeador.INSTANCE.mapUsuario(crearUsuarioDtoReq);
 		usuario.setRol(rol);
-		usuario.setActivo(true);
-		usuario.setCorreo(crearUsuarioDto.getCorreo());
-		usuario.setClave(crearUsuarioDto.getClave());
-		usuario.setFechaRegistro(LocalDate.now());
+		Usuario usuarioGuardado = usuarioRepository.save(usuario);       
+		return Mapeador.INSTANCE.mapUsuario(usuarioGuardado);
 
-
-		Usuario usuarioGuardado = usuarioRepository.save(usuario);
-
-		CrearUsuarioDtoResp crearUsuarioDtoResp = new  CrearUsuarioDtoResp();
-
-		crearUsuarioDtoResp.setIdentificacion(usuarioGuardado.getIdentificacion());
-		crearUsuarioDtoResp.setNombres(usuarioGuardado.getNombres());
-		crearUsuarioDtoResp.setApellidos(usuarioGuardado.getApellidos());
-		crearUsuarioDtoResp.setRol(usuarioGuardado.getRol().getDescripcion());
-		crearUsuarioDtoResp.setCorreo(usuarioGuardado.getCorreo());
-
-		return crearUsuarioDtoResp;
 	}
 
 	@Override
